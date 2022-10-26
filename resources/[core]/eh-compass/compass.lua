f5c92634891692aad08668d1e4fa581aaeed8d86
Gravity <bazookaforreal@gmail.com>
2022-10-26 16:59:39 -0400

    fix to make sure compass only in vehicle the batch file was for the old repo lmao

--- resources/[core]/eh-compass/compass.lua
+++ resources/[core]/eh-compass/compass.lua
@@ -1,94 +1,95 @@
 local compass = Config.compass
-Citizen.Trace("test\n")
+
 Citizen.CreateThread(function()
     if compass.position.centered then
         compass.position.x = compass.position.x - compass.width / 2
     end
 
     -- compass will always show when player is in a vehicle
-    while IsPedInAnyVehicle(PlayerPedId(), false) do
+    while true do
         Wait(0)
-        Citizen.Trace('in vehicle')
-        local pxDegree = compass.width / compass.fov
-        local playerHeadingDegrees = 0
+        if IsPedInAnyVehicle(PlayerPedId(), false) then
+            local pxDegree = compass.width / compass.fov
+            local playerHeadingDegrees = 0
 
-        if compass.followGameplayCam then
-            -- Converts [-180, 180] to [0, 360] where E = 90 and W = 270
-            local camRot = GetGameplayCamRot(0, Citizen.ResultAsVector())
-            playerHeadingDegrees = 360.0 - ((camRot.z + 360.0) % 360.0)
-        else
-            -- Converts E = 270 to E = 90
-            playerHeadingDegrees = 360.0 - GetEntityHeading(GetPlayerPed(-1))
-        end
+            if compass.followGameplayCam then
+                -- Converts [-180, 180] to [0, 360] where E = 90 and W = 270
+                local camRot = GetGameplayCamRot(0, Citizen.ResultAsVector())
+                playerHeadingDegrees = 360.0 - ((camRot.z + 360.0) % 360.0)
+            else
+                -- Converts E = 270 to E = 90
+                playerHeadingDegrees = 360.0 - GetEntityHeading(GetPlayerPed(-1))
+            end
 
-        local tickDegree = playerHeadingDegrees - compass.fov / 2
-        local tickDegreeRemainder = compass.ticksBetweenCardinals - (tickDegree % compass.ticksBetweenCardinals)
-        local tickPosition = compass.position.x + tickDegreeRemainder * pxDegree
+            local tickDegree = playerHeadingDegrees - compass.fov / 2
+            local tickDegreeRemainder = compass.ticksBetweenCardinals - (tickDegree % compass.ticksBetweenCardinals)
+            local tickPosition = compass.position.x + tickDegreeRemainder * pxDegree
 
-        tickDegree = tickDegree + tickDegreeRemainder
+            tickDegree = tickDegree + tickDegreeRemainder
 
-        while tickPosition < compass.position.x + compass.width do
-            if (tickDegree % 90.0) == 0 then
-                -- Draw cardinal
-                if compass.cardinal.tickShow then
-                    DrawRect(tickPosition,
-                        compass.position.y,
-                        compass.cardinal.tickSize.w,
-                        compass.cardinal.tickSize.h,
-                        compass.cardinal.tickColour.r,
-                        compass.cardinal.tickColour.g,
-                        compass.cardinal.tickColour.b,
-                        compass.cardinal.tickColour.a)
-                end
+            while tickPosition < compass.position.x + compass.width do
+                if (tickDegree % 90.0) == 0 then
+                    -- Draw cardinal
+                    if compass.cardinal.tickShow then
+                        DrawRect(tickPosition,
+                            compass.position.y,
+                            compass.cardinal.tickSize.w,
+                            compass.cardinal.tickSize.h,
+                            compass.cardinal.tickColour.r,
+                            compass.cardinal.tickColour.g,
+                            compass.cardinal.tickColour.b,
+                            compass.cardinal.tickColour.a)
+                    end
 
-                drawText(degreesToIntercardinalDirection(tickDegree), 
-                    tickPosition,
-                    compass.position.y + compass.cardinal.textOffset, {
-                        size = compass.cardinal.textSize,
-                        colour = compass.cardinal.textColour,
-                        outline = true,
-                        centered = true
-                    }
-                )
-            elseif (tickDegree % 45.0) == 0 and compass.intercardinal.show then
-                -- Draw intercardinal
-                if compass.intercardinal.tickShow then
-                    DrawRect(tickPosition,
-                        compass.position.y,
-                        compass.intercardinal.tickSize.w,
-                        compass.intercardinal.tickSize.h,
-                        compass.intercardinal.tickColour.r,
-                        compass.intercardinal.tickColour.g,
-                        compass.intercardinal.tickColour.b,
-                        compass.intercardinal.tickColour.a)
-                end
-
-                if compass.intercardinal.textShow then
-                    drawText(degreesToIntercardinalDirection(tickDegree),
+                    drawText(degreesToIntercardinalDirection(tickDegree), 
                         tickPosition,
-                        compass.position.y + compass.intercardinal.textOffset, {
-                            size = compass.intercardinal.textSize,
-                            colour = compass.intercardinal.textColour,
+                        compass.position.y + compass.cardinal.textOffset, {
+                            size = compass.cardinal.textSize,
+                            colour = compass.cardinal.textColour,
                             outline = true,
                             centered = true
                         }
                     )
-                end
-            else
-                -- Draw tick
-                DrawRect(tickPosition,
-                    compass.position.y,
-                    compass.tickSize.w,
-                    compass.tickSize.h,
-                    compass.tickColour.r,
-                    compass.tickColour.g,
-                    compass.tickColour.b,
-                    compass.tickColour.a)
-            end
+                elseif (tickDegree % 45.0) == 0 and compass.intercardinal.show then
+                    -- Draw intercardinal
+                    if compass.intercardinal.tickShow then
+                        DrawRect(tickPosition,
+                            compass.position.y,
+                            compass.intercardinal.tickSize.w,
+                            compass.intercardinal.tickSize.h,
+                            compass.intercardinal.tickColour.r,
+                            compass.intercardinal.tickColour.g,
+                            compass.intercardinal.tickColour.b,
+                            compass.intercardinal.tickColour.a)
+                    end
 
-            -- Advance to the next tick
-            tickDegree = tickDegree + compass.ticksBetweenCardinals
-            tickPosition = tickPosition + pxDegree * compass.ticksBetweenCardinals
+                    if compass.intercardinal.textShow then
+                        drawText(degreesToIntercardinalDirection(tickDegree),
+                            tickPosition,
+                            compass.position.y + compass.intercardinal.textOffset, {
+                                size = compass.intercardinal.textSize,
+                                colour = compass.intercardinal.textColour,
+                                outline = true,
+                                centered = true
+                            }
+                        )
+                    end
+                else
+                    -- Draw tick
+                    DrawRect(tickPosition,
+                        compass.position.y,
+                        compass.tickSize.w,
+                        compass.tickSize.h,
+                        compass.tickColour.r,
+                        compass.tickColour.g,
+                        compass.tickColour.b,
+                        compass.tickColour.a)
+                end
+
+                -- Advance to the next tick
+                tickDegree = tickDegree + compass.ticksBetweenCardinals
+                tickPosition = tickPosition + pxDegree * compass.ticksBetweenCardinals
+            end
         end
     end
 end)
@@ -172,5 +173,7 @@ function degreesToIntercardinalDirection(dgr)
         return "W"
     elseif dgr >= 292.5 and dgr < 337.5 then
         return "NW"
+    else
+        return "error"
     end
 end
