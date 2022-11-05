1a05c9ed131ded0dfb9bf181935cf72f385d1019
Gravity <bazookaforreal@gmail.com>
2022-11-04 22:45:14 -0400

    got notifications working

--- resources/[core]/eh-notify/client.lua
+++ resources/[core]/eh-notify/client.lua
@@ -1,16 +1,37 @@
+--[[
+    Available styles:
+    info
+    error
+    warning
+    success
+    message
+]]
+
 --- Sends a notification to the client
 --- @param _style string the style of notification
 --- @param _duration number the length in seconds for the notification
 --- @param _title string the title to display on the notification
---- @param _message string the message to send to the 
+--- @param _message string the message to send to the
 function Notify(_style, _duration, _title, _message)
     SendNUIMessage({
         type = 'noti',
         style = _style,
-        duration = _duration,
+        duration = _duration, -- js needs duration in ms; adjust to account
         title = _title,
         message = _message
     })
 end
 
 exports("Notify", Notify)
+
+
+local initObject = {
+    type = 'init',
+    position = 'top-right',
+    insertAnim = 'insert-right',
+    insertDuration = 1000,
+    removeAnim = 'fadeout',
+    removeDuration = 600,
+    maxNotifications = 0
+}
+SendNUIMessage(initObject)
--- resources/[core]/eh-vehicle/seatbelt.lua
+++ resources/[core]/eh-vehicle/seatbelt.lua
@@ -7,14 +7,14 @@ RegisterCommand('+seatbelt', function()
     if IsPedInAnyVehicle(PlayerPedId(), false) then
         if seatbeltOn then
             seatbeltOn = false
-            exports['eh-notify']:Notify('success', 'Seatbelt removed.')
+            exports['eh-notify']:Notify('info', 1, 'Seatbelt', 'Seatbelt removed.')
             SendNUIMessage({
                 type = 'seatbelt-toggle',
                 display = 'true'
             })
         else
             seatbeltOn = true
-            exports['eh-notify']:Notify('success', 'Seatbelt enabled.')
+            exports['eh-notify']:Notify('info', 1, 'Seatbelt', 'Seatbelt enabled.')
             SendNUIMessage({
                 type = 'seatbelt-toggle',
                 display = 'false'
@@ -32,7 +32,7 @@ Citizen.CreateThread(function()
         -- take the seatbelt off if the player gets out of the car with it on
         if seatbeltOn and not IsPedInAnyVehicle(PlayerPedId(), false) then
             seatbeltOn = false
-            exports['eh-notify']:Notify('success', 'Seatbelt removed.')
+            exports['eh-notify']:Notify('warning', 2, 'Seatbelt', 'Seatbelt removed. Exited vehicle.')
             SendNUIMessage({
                 type = 'seatbelt-toggle',
                 display = 'false'
@@ -55,7 +55,7 @@ Citizen.CreateThread(function()
                 SetEntityCoords(PlayerPedId(), coords.x + forwardV.x, coords.y + forwardV.y, coords.z - 0.47, true, true, true, false)
                 SetEntityVelocity(PlayerPedId(), lastVelocity.x, lastVelocity.y, lastVelocity.z)
                 SetPedToRagdoll(PlayerPedId(), 1000, 1000, 0, false, false, false)
-                exports['eh-notify']:Notify('error', 'You were ejected. Wear a seatbelt next time.')
+                exports['eh-notify']:Notify('error', 4, 'Seatbelt', 'You were ejected. Wear a seatbelt next time.')
 
                 -- clear speed after crash
                 lastSpeed = 0.0
