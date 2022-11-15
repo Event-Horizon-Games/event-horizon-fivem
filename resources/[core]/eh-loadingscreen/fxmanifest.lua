1fadba5935a4d08231cbe3031b37bfe8c54d67cc
Gravity <bazookaforreal@gmail.com>
2022-11-15 18:37:09 -0500

    refactored loading screen

--- resources/[core]/eh-loadingscreen/fxmanifest.lua
+++ resources/[core]/eh-loadingscreen/fxmanifest.lua
@@ -1,19 +1,19 @@
 -- This resource is part of the default Cfx.re asset pack (cfx-server-data)
 -- Altering or recreating for local use only is strongly discouraged.
-fx_version 'bodacious'
-game 'gta5'
 
 version '1.0.0'
-author 'Gravity'
+author 'me'
 description 'This is my loading screen'
 
-loadscreen 'html/index.html'
-
 files {
-    'html/index.html',
-    'html/style.css',
-    'html/script.js',
-    'html/fonts/bankgothic.ttf',
-    'html/images/loadscreen.jpg',
+    'index.html',
+    'keks.css',
+    'bankgothic.ttf',
+    'loadscreen.jpg',
     'music/space-jazz.mp3'
 }
+
+loadscreen 'index.html'
+
+fx_version 'bodacious'
+game 'gta5'
