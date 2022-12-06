7fafad06457a45d739bc00d07825352ffb5ee2d6
Gravity <bazookaforreal@gmail.com>
2022-12-05 20:43:40 -0500

    sdfsdfa

---
+++ start.bat
@@ -0,0 +1,4 @@
+"path-to-project-repoository" +exec server.cfg +set onesync on +set onesync_population true +set onesync_forceMigration 1 +set onesync_workaround763185 true +set onesync_distanceCullVehicles true +set sv_enforceGameBuild 2372
+:: 2372 is Los Santos Tuners
+:: will be replaced with wherever your repository is located
+:: ex. C:\Documents\repository
