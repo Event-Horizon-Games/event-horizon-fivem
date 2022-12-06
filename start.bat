d79ca5d07005ae2fff32a2ba04f0ff325a0f2f54
Gravity <bazookaforreal@gmail.com>
2022-12-05 20:46:42 -0500

    change start

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 "path-to-project-repoository" +exec server.cfg +set onesync on +set onesync_population true +set onesync_forceMigration 1 +set onesync_workaround763185 true +set onesync_distanceCullVehicles true +set sv_enforceGameBuild 2372
+:: "path-to-project-repository" will be replaced with the root folder of the repository of your local system
+:: ex. "C:\Documents\repository"
 :: 2372 is Los Santos Tuners
-:: will be replaced with wherever your repository is located
-:: ex. C:\Documents\repository
