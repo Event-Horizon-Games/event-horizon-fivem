3e0ac3e86cbbec1a4d28b2ee6d7bcd2253a902df
Gravity <bazookaforreal@gmail.com>
2023-01-11 23:21:31 -0500

    start working on scenes

---
+++ resources/[core]/eh-scenes/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Roleplay Scenes'
+description 'Sets up hover text players can place.'
+author 'Gravity'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
