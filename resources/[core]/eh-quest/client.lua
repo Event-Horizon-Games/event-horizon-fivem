c5d587cd04f2a04346d84c0feb4903b621824d6e
Gravity <bazookaforreal@gmail.com>
2024-08-23 01:29:33 -0400

    got the hide and display working

--- resources/[core]/eh-quest/client.lua
+++ resources/[core]/eh-quest/client.lua
@@ -1,6 +1,16 @@
+RegisterCommand("start", function()
+    Start()
+end)
+
 RegisterCommand("check", function()
     Check()
 end)
+
+function Start()
+    SendNUIMessage({
+        action = 'start',
+    })
+end
 
 function Check()
     SendNUIMessage({
--- resources/[core]/eh-quest/html/script.js
+++ resources/[core]/eh-quest/html/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
     window.addEventListener('message', function (event) {
         let data = event.data
-        if (data.action == 'check') {
+        if (data.action == 'start') {
+            document.getElementById("checkbox").style.display = "flex";
+        }
+        else if (data.action == 'check') {
             document.getElementById("one").checked = true;
         }
     });
